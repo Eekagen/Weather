67fbb204e2f79de575a4961e156ea70ce870c0d9
Eekagen <shunkan02@gmail.com>
2022-11-05 21:55:47 -0400

    PuckedFigs

--- weather.py
+++ weather.py
@@ -3,24 +3,41 @@ import random
 import datetime
 from boto3.dynamodb.conditions import Key
 
-ts = datetime.datetime.now()
-Timestamp = int(ts.timestamp() * 1000)
+class Weather:
+    zip = 1
+    humidity = 1
+    temperature = 1
 
+    def __init__(self, zip, humidity, temperature):
+        self.zip = zip
+        self.humidity = humidity
+        self.temperature = temperature
 
 dynamoDb = boto3.resource('dynamodb')
 
 tables = dynamoDb.tables.all()
 
-
-def GenerateTemp():
+def GenerateTemp(weather):
+    ts = datetime.datetime.now()
+    timestamp = int(ts.timestamp() * 1000)
     tabled = dynamoDb.Table('Puck_Figs')
     tabled.put_item(
     Item={
-        'zipcode': '20166',
-        'Humidity': '56',
-        'Temperature': '74',
-        'Timestamp': Timestamp,        
+        'zipcode': str(weather.zip),
+        'Humidity': weather.humidity,
+        'Temperature': weather.temperature,
+        'Timestamp': timestamp,        
     })
 
-GenerateTemp()
+def Florida():
+    genhumid = random.randint(44, 99)
+    gentemp = random.randint(30, 94)
+    return Weather(33765, genhumid, gentemp)
 
+def Virginia():
+    genhumid = random.randint(44, 99)
+    gentemp = random.randint(30, 94)
+    return Weather(20166, genhumid, gentemp)
+
+GenerateTemp(Virginia())
+GenerateTemp(Florida())
